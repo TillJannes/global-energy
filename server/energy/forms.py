23e7e6ea4e0ffb81915be29b2c37de244f80627b
Till Meinel <tilljannesmeinel@gmail.com>
2024-12-21 22:25:28 +0100

    feat: adding django-bootstrap5 and bootstrap-crisp

--- server/energy/forms.py
+++ server/energy/forms.py
@@ -4,4 +4,15 @@ from .models import Document
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
-        fields = ('description', 'document', )
+        fields = (
+            'description',
+            'document',
+        )
+        labels = {
+            "description": "",
+            "docmument" : "",
+        }
+        widgets = {
+            "description": forms.TextInput(attrs={"class": "form-control", "placeholder": "Description"}),
+            "document": forms.FileInput(attrs={"class": "form-control"}),
+        }
--- server/energy/views.py
+++ server/energy/views.py
@@ -1,19 +1,26 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 from .forms import DocumentForm
 
 
 def home(request: HttpRequest):
-    return HttpResponse("Moin, welcome to global energy.")
+    return render(request, template_name="home.html")
 
-def upload(request: HttpRequest):
+def upload(request: HttpRequest) -> HttpResponseRedirect | HttpResponse:
+    submitted = False
     if request.method == 'POST':
         form = DocumentForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('energy-home')
+            return HttpResponseRedirect("/upload?submitted=True")
     else:
         form = DocumentForm()
+        if "submitted" in request.GET:
+            submitted = True
     return render(request=request, template_name='upload.html', context={
-        'form': form
+        'form': form,
+        "submitted": submitted,
     })
+
+def insert(request: HttpRequest):
+    pass

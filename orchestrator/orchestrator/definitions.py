e2466ac39120e35fe266d038cb7cfefd44b2b1cb
Till Meinel <tilljannesmeinel@gmail.com>
2024-12-24 12:17:30 +0100

    chore: setting up file storage

--- orchestrator/orchestrator/definitions.py
+++ orchestrator/orchestrator/definitions.py
@@ -1,9 +1,16 @@
 from dagster import Definitions, load_assets_from_modules
+from .jobs import say_hello_pipeline
 
-from orchestrator import assets  # noqa: TID252
+from orchestrator.orchestrator.assets import greet
 
-all_assets = load_assets_from_modules([assets])
+all_assets = load_assets_from_modules(
+    [
+        greet,
+    ]
+)
+all_jobs = [say_hello_pipeline]
 
 defs = Definitions(
     assets=all_assets,
+    jobs=all_jobs,
 )

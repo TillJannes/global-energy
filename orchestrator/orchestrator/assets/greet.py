12dc7b57735acbcf90529031de44ad2abbc0187c
Till Meinel <tilljannesmeinel@gmail.com>
2025-03-28 21:37:27 +0100

    chore: adding pandera power timeseries model

--- orchestrator/orchestrator/assets/greet.py
+++ orchestrator/orchestrator/assets/greet.py
@@ -3,7 +3,12 @@ from dagster import (
     AssetExecutionContext,
     asset,
 )
+from orchestrator.orchestrator.data.table_types import PowerTimeseries
+from dagster_pandera import pandera_schema_to_dagster_type
 
-@asset(kinds=["python"])
+@asset(
+    kinds=["python"],
+    dagster_type=pandera_schema_to_dagster_type(PowerTimeseries)
+    )
 def greet(context: AssetExecutionContext):
     context.log.info(f"Hey there, current local timestamp is {datetime.datetime.now()}.")
